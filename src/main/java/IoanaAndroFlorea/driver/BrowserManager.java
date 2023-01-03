213d8615fae6fdae4ee7db79b894af7ea32baee2
Ioana Andronache-Florea <ioana.florea88@yahoo.com>
2023-01-03 19:06:07 +0200

    Initial commit

---
+++ src/main/java/IoanaAndroFlorea/driver/BrowserManager.java
@@ -0,0 +1,13 @@
+package IoanaAndroFlorea.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserManager {
+
+    public static ChromeDriver createChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+}
+
---
+++ src/test/java/IoanaAndroFlorea/tests/seleniumpart1/Tema6Test.java
@@ -0,0 +1,8 @@
+package IoanaAndroFlorea.tests.seleniumpart1;
+
+public class Tema6Test {
+
+    public static void main(String[] args) {
+
+    }
+}
