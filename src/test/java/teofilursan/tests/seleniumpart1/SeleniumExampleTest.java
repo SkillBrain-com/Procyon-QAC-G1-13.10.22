645195390268f6898d30cebaa3461b4365a6386d
SanduBogdan95 <bogdan95.sandu@gmail.com>
2023-01-05 01:33:33 +0200

    Revert "Tema 5"
    This reverts commit f8d2d345d2bb0ff75780bffe3b5ff72257013202.

--- src/test/java/teofilursan/tests/seleniumpart1/SeleniumExampleTest.java
+++ src/test/java/teofilursan/tests/seleniumpart1/SeleniumExampleTest.java
@@ -15,19 +15,8 @@ public class SeleniumExampleTest {
         WebDriver driver = BrowserManager.createChromeDriver();
         driver.get("https://testpages.herokuapp.com/styled/index.html");
         WebElement firstLink = driver.findElement(By.id("basicpagetest"));
-        System.out.println("Primul link are textul: "+firstLink.getText());
         firstLink.click();
-        WebElement lastParagraph = driver.findElement(By.className("sub"));
-        System.out.println("Ultimul paragraf are textul: " + lastParagraph.getText());
-        WebElement indexLink = driver.findElement(By.xpath("//div[@class='page-navigation']//a"));
-        indexLink.click();
-        WebElement secondLink = driver.findElement(By.linkText("Element Attributes Examples"));
-        secondLink.click();
-        WebElement dynamicParagraph = driver.findElement(By.linkText("jsattributes"));
-        System.out.println("Next-id is: " + dynamicParagraph.getAttribute("nextid"));
-        WebElement attributeButton = driver.findElement(By.className("styled-click-button"));
-        attributeButton.click();
-        System.out.println("Next-id after click is: " + dynamicParagraph.getAttribute("nextid"));
+        System.out.println("Current url is : " + driver.getCurrentUrl());
         driver.quit();
     }
 }
