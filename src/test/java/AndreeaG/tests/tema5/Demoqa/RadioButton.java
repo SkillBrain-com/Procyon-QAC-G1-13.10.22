d77f6a8c11be4934c7b15b27e83bdad1bd4f98ff
redandreea <redandreea@gmail.com>
2023-01-11 17:19:41 +0200

    added more

--- src/test/java/AndreeaG/tests/tema5/Demoqa/RadioButton.java
+++ src/test/java/AndreeaG/tests/tema5/Demoqa/RadioButton.java
@@ -1,4 +1,40 @@
 package AndreeaG.tests.tema5.Demoqa;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import teofilursan.driver.BrowserManager;
+
 public class RadioButton {
+
+    public static void main(String[] args) {
+
+        radioButton();
+
+    }
+
+    public static void radioButton(){
+
+
+        ChromeDriver driver = BrowserManager.createChromeDriver();
+        driver.get("https://demoqa.com/radio-button");
+        WebElement radioButtonYes = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div[2]/div[2]/label"));
+        WebElement radioButtonImpressive =driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div[2]/div[3]/label"));
+        radioButtonYes.click();
+        radioButtonImpressive.click();
+
+
+        //driver.quit();
+
+
+
+    }
+
+
+
+
+
+
+
+
 }
