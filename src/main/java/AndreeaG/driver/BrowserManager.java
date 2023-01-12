523d4d7a6929a36a6808cf514ee86a9d878652ec
redandreea <redandreea@gmail.com>
2023-01-12 23:48:52 +0200

    added the method that creates the driver in BrowserManager

--- src/main/java/AndreeaG/driver/BrowserManager.java
+++ src/main/java/AndreeaG/driver/BrowserManager.java
@@ -3,11 +3,11 @@ package AndreeaG.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-    public class BrowserManager {
+public class BrowserManager {
 
-        public static ChromeDriver createChromeDriver() {
-            WebDriverManager.chromedriver().setup();
-            return new ChromeDriver();
-        }
+    public static ChromeDriver createChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
     }
+}
 
