07360f1061900bdc3756d29473690b82e0569259
Fsorin <sorinfoca@yahoo.com>
2023-01-12 10:43:36 +0200

    Complet

--- src/test/java/sorinfoca/Tests/Tema6/HerokuAlerts.java
+++ src/test/java/sorinfoca/Tests/Tema6/HerokuAlerts.java
@@ -32,41 +32,49 @@ public class HerokuAlerts {
             closeBrowser();
         }
     }
+
     public static void navigateToHerokuHomePage() {
         driver = BrowserManager.createChromeDriver();
         driver.get("https://testpages.herokuapp.com/styled/index.html");
         System.out.println("Opened Heroku index page");
     }
+
     public static void openAlertsPage() {
         WebElement alertsLink = driver.findElement(By.id("alerttest"));
         alertsLink.click();
         System.out.println("Navigated to alerts page");
     }
+
     public static void openAlert() {
         WebElement secondAlertButton = driver.findElement(By.xpath("//input[@id='confirmexample']"));
         secondAlertButton.click();
         System.out.println("Clicked on SecondAlertButton");
     }
+
     public static void acceptAlert() {
         Alert alert = driver.switchTo().alert();
         alert.accept();
         System.out.println("Accepted alert");
     }
+
     public static void dismissAlert() {
         Alert alert = driver.switchTo().alert();
         alert.dismiss();
         System.out.println("Dismissed alert");
     }
+
     public static void getTextOfAlert() {
         Alert alert = driver.switchTo().alert();
         String alertText = alert.getText();
         System.out.println("Alert text: " + alertText);
     }
+
     public static void inputTextInAlert() {
         Alert secondAlert = driver.switchTo().alert();
         secondAlert.sendKeys("text to input in prompt box");
         System.out.println("Entered text in alert box");
     }
+
     public static void handleDifferentTypeOfAlert() {
         Alert secondAlert = driver.switchTo().alert();
         String alertText = secondAlert.getText();
@@ -81,6 +89,7 @@ public class HerokuAlerts {
             System.out.println("Unknown type of alert: " + alertText);
         }
     }
+
     public static void findNonexistentElement() {
         try {
             WebElement nonExistentElement = driver.findElement(By.id("nonexistent-element"));
@@ -89,6 +98,7 @@ public class HerokuAlerts {
             System.out.println("Could not find the element with id 'nonexistent-element'");
         }
     }
+
     public static void closeBrowser() {
         driver.quit();
         System.out.println("Closed the browser");
