2d331dcada9f22dfdd30b9aca48d6ecf3c8ed3c7
ancapatricia <mrn.anca@yahoo.com>
2023-01-12 18:39:55 +0200

    am mai completat la tema6 (nu e gata inca)

--- src/test/java/ancaMarian/tests/tema6/HerokuAlerts.java
+++ src/test/java/ancaMarian/tests/tema6/HerokuAlerts.java
@@ -12,25 +12,36 @@ import java.io.IOException;
 public class HerokuAlerts {
     static ChromeDriver driver = null;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws InterruptedException {
     navigateToHerokuHomePage();
+    maximizeWindow(driver);
     openAlertsPage();
-    openAlert();
+    openFirstAlert();
+    acceptFirstAlert();
+    openSecondAlert();
     acceptAlert();
+    openThirdAlert();
+    completeThirdAlert();
     closeBrowser();
 
     navigateToHerokuHomePage();
     try {
-        openAlert();
+        openSecondAlert();
     } catch (NoSuchElementException e){
-        FileUtils.takeScreenshot(driver, "alert");
+        FileUtils.takeScreenshot(driver, "alertNotFound");
+        System.out.println("Nu s-a gasit elementul pe pagina. S-a salvat un screenshot.");
         } finally {
         closeBrowser();}
     }
+
     public static void navigateToHerokuHomePage(){
         driver = BrowserManager.createChromeDriver();
         driver.get("https://testpages.herokuapp.com/styled/index.html");
         System.out.println("Am deschis Heroku index page.");
+    }
+
+    public static void maximizeWindow(ChromeDriver driver){
+        driver.manage().window().maximize();
     }
 
     public static void openAlertsPage(){
@@ -39,16 +50,61 @@ public class HerokuAlerts {
         System.out.println("Am deschis Alert page.");
     }
 
-    public static void openAlert(){
-        WebElement secondAlertButton = driver.findElement(By.xpath("//input[@id='confirmexample']"));
-        secondAlertButton.click();
-        System.out.println("Am deschis alerta.");
+    public static void openFirstAlert() throws InterruptedException {
+        WebElement firstAlertButton = driver.findElement(By.xpath("//input[@id='alertexamples']"));
+        firstAlertButton.click();
+        Thread.sleep(2000);
+        System.out.println("Am deschis prima alerta.");
     }
 
-    public static void acceptAlert(){
+    public static void acceptFirstAlert() throws InterruptedException {
+        Alert firstAlert = driver.switchTo().alert();
+        firstAlert.accept();
+        Thread.sleep(2000);
+        System.out.println("Am acceptat prima alerta.");
+    }
+
+    public static void openSecondAlert() throws InterruptedException {
+        WebElement secondAlertButton = driver.findElement(By.xpath("//input[@id='confirmexample']"));
+        Thread.sleep(2000);
+        secondAlertButton.click();
+        System.out.println("Am deschis a doua alerta.");
+    }
+
+    public static void acceptAlert() throws InterruptedException {
         Alert secondAlert = driver.switchTo().alert();
+        Thread.sleep(2000);
         secondAlert.accept();
-        System.out.println("Am acceptat alerta.");
+        WebElement confirmationOfAcceptance = driver.findElement(By.id("confirmreturn"));
+        if (confirmationOfAcceptance.getText().equals("true")) {
+            System.out.println("Am acceptat a doua alerta.");
+        } else {
+            System.out.println("Nu s-a efectuat acceptarea celei de-a doua alerte.");
+        }
+    }
+
+    public static void openThirdAlert() {
+        WebElement thirdAlertButton = driver.findElement(By.xpath("//input[@id='promptexample']"));
+        thirdAlertButton.click();
+        System.out.println("Am deschis a treia alerta.");
+    }
+
+    public static void completeThirdAlert() throws InterruptedException {
+        Thread.sleep(5000);
+        Alert thirdAlert = driver.switchTo().alert();
+        Thread.sleep(2000);
+        thirdAlert.sendKeys("do change me!");
+        Thread.sleep(2000);
+        thirdAlert.accept();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,1000)");
+        System.out.println("Am facut scroll pana jos de tot.");
+        WebElement changeConfirmation = driver.findElement(By.id("promptreturn"));
+        if (changeConfirmation.getText().equals("do change me!")) {System.out.println("Am completat si am acceptat a treia alerta.");}
+        else {
+            System.out.println("nu s-a efectuat schimbarea!");
+        }
+        Thread.sleep(2000);
     }
 
     public static void closeBrowser(){
--- src/test/java/ancaMarian/tests/tema6/HerokuForms.java
+++ src/test/java/ancaMarian/tests/tema6/HerokuForms.java
@@ -1,6 +1,8 @@
 package ancaMarian.tests.tema6;
 
+import ancaMarian.utils.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -10,16 +12,106 @@ public class HerokuForms {
 
     static ChromeDriver driver = null;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        navigateToHerokuHomePage();
+        maximizeWindow(driver);
         navigateToHerokuFormsPage();
+        inputUsername();
+        inputPassword();
+        inputComment();
+        addFile();
+        selectCheckbox();
+        deselectCheckbox();
+        selectRadioItem();
+        selectFromMultipleValues();
+        selectFromDropdownList();
         scrollToSubmitButton();
-        closeBrowser();
+        FileUtils.takeScreenshot(driver, "form");
+        goBackToFormPage();
+        try {
+            goBackToFormPage();
+        } catch (NoSuchElementException e) {
+            FileUtils.takeScreenshot(driver, "ElementNotFound");
+            System.out.println("Nu s-a gasit elementul pe pagina. S-a salvat un screenshot.");
+        } finally {
+            closeBrowser();}
+    }
+
+    public static void navigateToHerokuHomePage(){
+        driver = ancaMarian.driver.BrowserManager.createChromeDriver();
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+        System.out.println("Am deschis Heroku index page.");
+    }
+
+    public static void maximizeWindow(ChromeDriver driver){
+        driver.manage().window().maximize();
     }
 
     public static void navigateToHerokuFormsPage() {
-        driver = BrowserManager.createChromeDriver();
-        driver.get("https://testpages.herokuapp.com/styled/basic-html-form-test.html");
-        System.out.println("Am deschis Heroku Forms page!");
+        WebElement formsPage = driver.findElement(By.id("htmlformtest"));
+        formsPage.click();
+        System.out.println("Am deschis Heroku Forms page.");
+    }
+
+    public static void inputUsername(){
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("Username1");
+        System.out.println("Am adaugat un username.");
+    }
+
+    public static void inputPassword(){
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("password1");
+        System.out.println("Am adaugat o parola.");
+    }
+
+    public static void inputComment(){
+        WebElement comment = driver.findElement(By.name("comments"));
+        comment.clear();
+        comment.sendKeys("comment1");
+        System.out.println("Am adaugat un comentariu.");
+    }
+
+    public static void addFile(){
+        WebElement uploadFile = driver.findElement(By.name("filename"));
+        uploadFile.sendKeys("/Users/ancapatriciamarian/Desktop/procyon/Logs/alertNotFound.png");
+        System.out.println("Am adagat un fisier.");
+    }
+
+    public static void selectCheckbox() throws InterruptedException {
+        WebElement checkbox = driver.findElement(By.xpath("//*[@id='HTMLFormElements']/table/tbody/tr[5]/td/input[1]"));
+        checkbox.click();
+        Thread.sleep(2000);
+        System.out.println("Am selectat un checkbox.");
+    }
+
+    public static void deselectCheckbox() throws InterruptedException {
+        WebElement deselectCheckbox = driver.findElement(By.xpath("//*[@id='HTMLFormElements']/table/tbody/tr[5]/td/input[3]"));
+        deselectCheckbox.click();
+        Thread.sleep(2000);
+        System.out.println("Am deselectat un checkbox.");
+    }
+
+    public static void selectRadioItem() throws InterruptedException {
+        WebElement radioItem = driver.findElement(By.xpath("//*[@id='HTMLFormElements']/table/tbody/tr[6]/td/input[1]"));
+        radioItem.click();
+        Thread.sleep(2000);
+        System.out.println("Am selectat un radio button.");
+    }
+
+    public static void selectFromMultipleValues() throws InterruptedException {
+        WebElement multipleValues = driver.findElement(By.xpath("//*[@id='HTMLFormElements']/table/tbody/tr[7]/td/select/option[2]"));
+        multipleValues.click();
+        Thread.sleep(2000);
+        System.out.println("Am selectat a doua optiune din Multiple Select Values.");
+    }
+
+    public static void selectFromDropdownList() throws InterruptedException {
+        WebElement dropdownList = driver.findElement(By.name("dropdown"));
+        dropdownList.click();
+        Thread.sleep(2000);
+        WebElement secondElementInDropdownList = driver.findElement(By.xpath("//*[@id='HTMLFormElements']/table/tbody/tr[8]/td/select/option[2]"));
+        secondElementInDropdownList.click();
     }
 
     public static void scrollToSubmitButton() {
@@ -31,9 +123,15 @@ public class HerokuForms {
         System.out.println("Am facut scroll catre submit button");
     }
 
-    public static void closeBrowser() {
+    public static void goBackToFormPage(){
+        WebElement backToFormPage = driver.findElement(By.id("back_to_form"));
+        backToFormPage.click();
+        System.out.println("M-am intors la pagina de Form");
+    }
+
+    public static void closeBrowser() throws InterruptedException {
+        Thread.sleep(2000);
         driver.quit();
         System.out.println("Am inchis browserul!");
     }
-
 }
