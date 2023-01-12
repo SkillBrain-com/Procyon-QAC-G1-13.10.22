b663b3b6b1093678d1d501595d9c6805ee88edda
Fsorin <sorinfoca@yahoo.com>
2023-01-12 20:41:05 +0200

    TestNGTeorie

--- src/test/java/sorinfoca/teorie/DemoQaInputs.java
+++ src/test/java/sorinfoca/teorie/DemoQaInputs.java
@@ -9,32 +9,42 @@ import teofilursan.driver.BrowserManager;
 public class DemoQaInputs {
     ChromeDriver driver;
 
-
     @Test
     public void test1() {
         driver = BrowserManager.createChromeDriver();
         driver.get("https://demoqa.com/text-box");
         writeFullName();
+        writeEmail();
+        writeCurrentAddress();
+        writePermanentAddress();
+        clickOnSubmitButton();
         driver.quit();
     }
 
     public void writeFullName() {
         WebElement fullNameInput = driver.findElement(By.id("userName"));
-        fullNameInput.sendKeys("Foca Sorin");
+        fullNameInput.sendKeys("Teofil Ursan");
     }
 
-    public void writeEmail(){
+    public void writeEmail() {
         WebElement emailInput = driver.findElement(By.id("userEmail"));
-        emailInput.sendKeys("sorin@test.com");
+        emailInput.sendKeys("teo@test.com");
     }
 
     public void writeCurrentAddress() {
-        WebElement currentAddressInput = driver.findElement(By.id("currentAddress"));
+        WebElement currentAddressInput = driver
+                .findElement(By.id("currentAddress"));
         currentAddressInput.sendKeys("Iasi");
     }
 
     public void writePermanentAddress() {
-        WebElement permanentAddressInput = driver.findElement(By.id("permanentAddress"));
+        WebElement permanentAddressInput = driver
+                .findElement(By.id("permanentAddress"));
         permanentAddressInput.sendKeys("Iasi");
+    }
+
+    public void clickOnSubmitButton() {
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        submitButton.click();
     }
 }
