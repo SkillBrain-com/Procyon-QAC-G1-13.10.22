d692d04f93b685c9ca0816e97a24217973180ea1
Teo <ursan.teofil@yahoo.com>
2023-01-12 21:30:07 +0200

    Suite-test.xml file

---
+++ src/test/java/teofilursan/teorie/altpachet/AltTest.java
@@ -0,0 +1,11 @@
+package teofilursan.teorie.altpachet;
+
+import org.testng.annotations.Test;
+
+public class AltTest {
+
+    @Test
+    public void writeMessage() {
+        System.out.println("Mesajul meu din AltTest class!");
+    }
+}
