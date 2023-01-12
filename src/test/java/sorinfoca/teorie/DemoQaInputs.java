1bd288754bcacfc95c04afde6e9bfb41571a69a5
Fsorin <sorinfoca@yahoo.com>
2023-01-12 20:04:21 +0200

    teorie1

--- src/test/java/sorinfoca/teorie/DemoQaInputs.java
+++ src/test/java/sorinfoca/teorie/DemoQaInputs.java
@@ -3,9 +3,20 @@ package sorinfoca.teorie;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import teofilursan.driver.BrowserManager;
 
 public class DemoQaInputs {
     ChromeDriver driver;
+
+
+    @Test
+    public void test1() {
+        driver = BrowserManager.createChromeDriver();
+        driver.get("https://demoqa.com/text-box");
+        writeFullName();
+        driver.quit();
+    }
 
     public void writeFullName() {
         WebElement fullNameInput = driver.findElement(By.id("userName"));
