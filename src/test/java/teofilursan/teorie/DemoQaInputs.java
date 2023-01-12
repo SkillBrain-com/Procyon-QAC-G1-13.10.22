e21cbb6beecb7a30b616d35bd767954137b3195b
Teo <ursan.teofil@yahoo.com>
2023-01-12 20:08:07 +0200

    First test added

--- src/test/java/teofilursan/teorie/DemoQaInputs.java
+++ src/test/java/teofilursan/teorie/DemoQaInputs.java
@@ -3,9 +3,22 @@ package teofilursan.teorie;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import teofilursan.driver.BrowserManager;
 
 public class DemoQaInputs {
     ChromeDriver driver;
+
+    @Test
+    public void test1() {
+        driver = BrowserManager.createChromeDriver();
+        driver.get("https://demoqa.com/text-box");
+        writeFullName();
+        writeEmail();
+        writeCurrentAddress();
+        writePermanentAddress();
+        driver.quit();
+    }
 
     public void writeFullName() {
         WebElement fullNameInput = driver.findElement(By.id("userName"));
