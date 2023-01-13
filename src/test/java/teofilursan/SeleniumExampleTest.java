422c8d42b9ea258d42908c11bb1b7b80218f23b6
SanduBogdan95 <bogdan95.sandu@gmail.com>
2023-01-13 21:58:40 +0200

    Am adaugat clasele sterse

---
+++ src/test/java/teofilursan/SeleniumExampleTest.java
@@ -0,0 +1,22 @@
+package teofilursan;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import teofilursan.driver.BrowserManager;
+
+public class SeleniumExampleTest {
+
+    public static void main(String[] args) {
+        basicPageTest();
+    }
+
+    public static void basicPageTest() {
+        WebDriver driver = BrowserManager.createChromeDriver();
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+        WebElement firstLink = driver.findElement(By.id("basicpagetest"));
+        firstLink.click();
+        System.out.println("Current url is : " + driver.getCurrentUrl());
+        driver.quit();
+    }
+}
