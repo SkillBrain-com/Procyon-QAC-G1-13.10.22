62360e9bbecde41c8c5feda98e2f66046f1098a1
redandreea <redandreea@gmail.com>
2023-01-13 10:49:51 +0200

    Removed blank lines

--- src/test/java/AndreeaG/tests/tema5/Demoqa/CheckBox.java
+++ src/test/java/AndreeaG/tests/tema5/Demoqa/CheckBox.java
@@ -6,19 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import teofilursan.driver.BrowserManager;
 
 public class CheckBox {
-
     public static void main(String[] args) {
         goToCheckbox1();
-
     }
     public static void goToCheckbox1() {
-
         ChromeDriver driver = BrowserManager.createChromeDriver();
         driver.get("https://demoqa.com/checkbox");
-
         WebElement checkBox = driver.findElement(By.xpath("//label/span[1]"));
         checkBox.click();
-
         WebElement expandPlusSign =driver.findElement(By.cssSelector(".rct-icon.rct-icon-expand-all"));
         expandPlusSign.click();
         driver.quit();
