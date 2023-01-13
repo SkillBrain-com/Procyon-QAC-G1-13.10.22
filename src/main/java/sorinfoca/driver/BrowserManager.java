79f1b9330586ba3b466448bbaab7e1c5c79757ad
Fsorin <sorinfoca@yahoo.com>
2023-01-13 02:41:00 +0200

    Commit SemifinalV1

--- src/main/java/sorinfoca/driver/BrowserManager.java
+++ src/main/java/sorinfoca/driver/BrowserManager.java
@@ -1,4 +1,12 @@
 package sorinfoca.driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BrowserManager {
+
+    public static ChromeDriver createChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
 }
--- src/test/java/sorinfoca/tests/MyTests.java
+++ src/test/java/sorinfoca/tests/MyTests.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import sorinfoca.driver.DriverSetup;
+import sorinfoca.driver.BrowserManager;
+
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
 
@@ -15,73 +16,75 @@ public class MyTests extends TestConfig {
 
     @BeforeEach
     public void beforeEach() {
-        driver = DriverSetup.setupDriver();
+        driver = BrowserManager.createChromeDriver();
     }
 
     @Test
     public void testValidLogin() {
         driver.get(getBaseUrl());
-        driver.findElement(By.cssSelector("#menu-right > ul > li:nth-child(2) > a")).click();
-        driver.findElement(By.name("username")).sendKeys("valid_username");
-        driver.findElement(By.name("password")).sendKeys("valid_password");
+        driver.findElement(By.cssSelector("#menu-toggle")).click();
+        driver.findElement(By.cssSelector("#sidebar-wrapper > ul > li:nth-child(4) > a")).click();
+        driver.findElement(By.cssSelector("#txt-username")).sendKeys("John Doe");
+        driver.findElement(By.name("password")).sendKeys("ThisIsNotAPassword");
         driver.findElement(By.xpath("//button[text()='Login']")).click();
-        assertTrue(driver.findElement(By.cssSelector("#menu-right > ul > li.dropdown.open > ul > li:nth-child(1) > a")).isDisplayed());
+        assertTrue(driver.findElement(By.cssSelector("#menu-toggle")).isDisplayed());
     }
 
     @Test
     public void testInvalidLogin() {
         driver.get(getBaseUrl());
-        driver.findElement(By.cssSelector("#menu-right > ul > li:nth-child(2) > a")).click();
-        driver.findElement(By.name("username")).sendKeys("invalid_username");
+        driver.findElement(By.cssSelector("#menu-toggle")).click();
+        driver.findElement(By.cssSelector("#sidebar-wrapper > ul > li:nth-child(4) > a")).click();
+        driver.findElement(By.cssSelector("#txt-username")).sendKeys("invalid_username");
         driver.findElement(By.name("password")).sendKeys("invalid_password");
         driver.findElement(By.xpath("//button[text()='Login']")).click();
-        assertTrue(driver.findElement(By.cssSelector(".alert.alert-danger")).isDisplayed());
+        assertTrue(driver.findElement(By.cssSelector("#login > div > div > div.col-sm-12.text-center > p.lead.text-danger")).isDisplayed());
     }
 
     @Test
     public void testRedirectToLogin() {
         driver.get(getBaseUrl());
-        driver.findElement(By.cssSelector("#menu-right > ul > li:nth-child(4) > a")).click();
+        driver.findElement(By.cssSelector("#menu-toggle")).click();
+        driver.findElement(By.cssSelector("#sidebar-wrapper > ul > li:nth-child(4) > a")).click();
         assertEquals(getBaseUrl() + "/auth/login", driver.getCurrentUrl());
-    }
+        }
+
 
     @Test
     public void testPositiveBookingScenario() {
         driver.get(getBaseUrl());
-        // login as valid user
-        driver.findElement(By.cssSelector("#menu-right > ul > li:nth-child(2) > a")).click();
-        driver.findElement(By.name("username")).sendKeys("valid_username");
-        driver.findElement(By.name("password")).sendKeys("valid_password");
+        driver.findElement(By.cssSelector("#menu-toggle")).click();
+        driver.findElement(By.cssSelector("#sidebar-wrapper > ul > li:nth-child(4) > a")).click();
+        driver.findElement(By.cssSelector("#txt-username")).sendKeys("John Doe");
+        driver.findElement(By.name("password")).sendKeys("ThisIsNotAPassword");
         driver.findElement(By.xpath("//button[text()='Login']")).click();
-        // navigate to appointment page
-        driver.findElement(By.cssSelector("#menu-right > ul > li:nth-child(4) > a")).click();
-        // fill out and submit appointment form
-        driver.findElement(By.name("name")).sendKeys("John Smith");
-        driver.findElement(By.name("email")).sendKeys("john.smith@example.com");
-        driver.findElement(By.name("date")).sendKeys("2022-12-25");
-        driver.findElement(By.name("symptoms")).sendKeys("Fever and cough");
-        driver.findElement(By.xpath("//button[text()='Book Appointment']")).click();
-        // verify successful booking message
-        assertTrue(driver.findElement(By.cssSelector(".alert.alert-success")).isDisplayed());
+        driver.findElement(By.cssSelector("#chk_hospotal_readmission")).click();
+        driver.findElement(By.cssSelector("#radio_program_medicaid")).click();
+        driver.findElement(By.cssSelector("#txt_visit_date")).sendKeys("10/10/23");
+        driver.findElement(By.cssSelector("#txt_comment")).sendKeys("Commentariu test");
+        driver.findElement(By.xpath("//*[@id=\"btn-book-appointment\"]")).click();
+        assertTrue(driver.findElement(By.cssSelector("#summary > div > div > div.col-xs-12.text-center > h2")).isDisplayed());
     }
 
     @Test
     public void testNegativeBookingScenario() {
         driver.get(getBaseUrl());
-        // navigate to appointment page
-        driver.findElement(By.cssSelector("#menu-right > ul > li:nth-child(4) > a")).click();
-        // fill out and submit appointment form with missing required fields
-        driver.findElement(By.name("name")).sendKeys("John Smith");
-        driver.findElement(By.name("email")).sendKeys("john.smith@example.com");
+        driver.findElement(By.cssSelector("#menu-toggle")).click();
+        driver.findElement(By.cssSelector("#sidebar-wrapper > ul > li:nth-child(4) > a")).click();
+        driver.findElement(By.cssSelector("#txt-username")).sendKeys("John Doe");
+        driver.findElement(By.name("password")).sendKeys("ThisIsNotAPassword");
+        driver.findElement(By.xpath("//button[text()='Login']")).click();
+        driver.findElement(By.cssSelector("#radio_program_medicaid")).click();
+        driver.findElement(By.cssSelector("#txt_comment")).sendKeys("Commentariu test negativ");
         driver.findElement(By.xpath("//button[text()='Book Appointment']")).click();
-        // verify error message
-        assertTrue(driver.findElement(By.cssSelector(".alert.alert-danger")).isDisplayed());
+        assertTrue(driver.findElement(By.cssSelector("#txt_visit_date")).isDisplayed());
     }
 
     @Test
     public void testHomeButton() {
-        driver.get(getBaseUrl() + "/path/to/page");
-        driver.findElement(By.cssSelector("#menu-right > ul > li:nth-child(1) > a")).click();
+        driver.get(getBaseUrl());
+        driver.findElement(By.cssSelector("#menu-toggle")).click();
+        driver.findElement(By.cssSelector("##sidebar-wrapper > ul > li:nth-child(3) > a")).click();
         assertEquals(getBaseUrl() + "/", driver.getCurrentUrl());
     }
 }
