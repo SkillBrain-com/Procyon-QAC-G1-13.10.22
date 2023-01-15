01f035cde6aff0fd7bb58a15e78c35a1c5c3c97b
SanduBogdan95 <bogdan95.sandu@gmail.com>
2023-01-15 23:30:03 +0200

    Add invalidLogin

--- src/test/java/bogdanSandu/tests/Tema7/CURAHealthcare.java
+++ src/test/java/bogdanSandu/tests/Tema7/CURAHealthcare.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-
 public class CURAHealthcare {
     static ChromeDriver driver = null;
 
@@ -50,7 +49,35 @@ public class CURAHealthcare {
         catch (AssertionError e) {
             System.out.println("Login failed!");
         }
-        driver.close();
+    }
+
+    @Test
+    public void invalidLogin(){
+        WebElement menu = driver.findElement(By.id("menu-toggle"));
+        menu.click();
+        System.out.println("Click on menu");
+        WebElement menuLogin = driver.findElement(By.xpath("//*[@id=\"sidebar-wrapper\"]/ul/li[3]/a"));
+        menuLogin.click();
+        System.out.println("Click on login menu");
+        // access elements of login page
+        WebElement username = driver.findElement(By.id("txt-username"));
+        WebElement password = driver.findElement(By.id("txt-password"));
+        WebElement login = driver.findElement(By.id("btn-login"));
+        // enter login data
+        username.sendKeys("Invalid Username");
+        System.out.println("Completed username");
+        password.sendKeys("ThisIsNotAPassword");
+        System.out.println("Completed password");
+        login.click();
+
+        String actualUrl = driver.getCurrentUrl();
+        String expectedUrl = "https://katalon-demo-cura.herokuapp.com/#appointment";
+        try {
+            Assert.assertEquals(expectedUrl,actualUrl);
+        }
+        catch (AssertionError e) {
+            System.out.println("Login failed!");
+        }
     }
 
     @AfterMethod
