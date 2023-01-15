e5a1686ebb610ab42475273960cd24af39923a84
Ioana Andronache-Florea <ioana.florea88@yahoo.com>
2023-01-15 11:07:47 +0200

    Tema 6 -inceput

---
+++ src/test/java/ioanadroflorea/tests/Tema6/HerokuForms.java
@@ -0,0 +1,12 @@
+package ioanadroflorea.tests.Tema6;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HerokuForms {
+
+    static ChromeDriver driver = null;
+
+    public static void main(String[] args) {
+        
+    }
+}
