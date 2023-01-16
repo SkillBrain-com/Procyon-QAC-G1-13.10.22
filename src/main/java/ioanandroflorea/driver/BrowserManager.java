f60c955db771120d2e730c97cef754ea6c14d5e0
Ioana Andronache-Florea <ioana.florea88@yahoo.com>
2023-01-16 22:43:07 +0200

    Tema 6 - BrowseManager modified

--- src/main/java/ioanandroflorea/driver/BrowserManager.java
+++ src/main/java/ioanandroflorea/driver/BrowserManager.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserManager {
+
     public static ChromeDriver createChromeDriver() {
         WebDriverManager.chromedriver().setup();
         return new ChromeDriver();
