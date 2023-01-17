989663380e4d39aabe6eaad96851b2aef26bb187
ancapatricia <mrn.anca@yahoo.com>
2023-01-17 17:02:48 +0200

    tema7 finalizata

--- src/test/java/ancaMarian/tests/tema7/Tests.java
+++ src/test/java/ancaMarian/tests/tema7/Tests.java
@@ -19,10 +19,11 @@ public class Tests extends Configurations {
     @DataProvider(name = "appointmentDetails")
     public Object[][] appointmentDetails(){
         return new Object[][]{
-                {"1/03/2023", "comment", true},
-                {"1/03/2023", " ", true},
-                {"1/03/2021", "comment", false}, //this test SHOULD FAIL, because it's an appointment in the past
-                {"", "comment", false}
+                {"1/03/2023", "comment", true}, //this test should pass
+                {"1/03/2023", " ", true}, //this test should pass
+                {"1/03/2021", "comment", false}, // this test SHOULD FAIL, because it's an appointment in the past
+                {"", "comment", false}, // this test should pass, it should NOT take me to Appointment Confirmation Page
+                {"", "", false} // this test should pass, it should NOT take me to Appointment Confirmation Page
         };
     }
 
@@ -76,12 +77,86 @@ public class Tests extends Configurations {
         WebElement bookAppointmentButton = driver.findElement(By.id("btn-book-appointment"));
         bookAppointmentButton.click();
         if (testShouldPass) {
-            Assert.assertEquals("Appointment Confirmation", driver.findElement(By.xpath("//*[@id='summary']/div/div/div[1]/h2")).getText());
+            Assert.assertEquals("https://katalon-demo-cura.herokuapp.com/appointment.php#summary", driver.getCurrentUrl());
         } else {
         Assert.assertNotEquals("https://katalon-demo-cura.herokuapp.com/appointment.php#summary", driver.getCurrentUrl());}
     }
 
+   @Test
+   public void returnToHomepage() {
+        returnToHomePageFromHomepageMenu();
+        closeDriver();
+        setupDriver();
+        returnToHomepageFromLoginPage();
+        closeDriver();
+        setupDriver();
+        returnToHomepageFromAppointmentFormPage();
+        closeDriver();
+        setupDriver();
+        returnToHomepageFromAppointmentConfirmationPage();
+   }
 
+    public void returnToHomePageFromHomepageMenu() {
+        driver.get(url);
+        WebElement menuButton = driver.findElement(By.id("menu-toggle"));
+        menuButton.click();
+        WebElement homeFromMenuInHomePage = driver.findElement(By.xpath("//*[@id='sidebar-wrapper']/ul/li[2]/a"));
+        homeFromMenuInHomePage.click();
+        Assert.assertEquals("https://katalon-demo-cura.herokuapp.com/", driver.getCurrentUrl());
     }
 
+    public void returnToHomepageFromLoginPage() {
+        driver.get(url);
+        WebElement menuButton = driver.findElement(By.id("menu-toggle"));
+        menuButton.click();
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id='sidebar-wrapper']/ul/li[3]/a"));
+        loginButton.click();
+        WebElement menuButtonFromLoginPage = driver.findElement(By.id("menu-toggle"));
+        menuButtonFromLoginPage.click();
+        WebElement homeFromMenuInLoginPage = driver.findElement(By.xpath("//*[@id='sidebar-wrapper']/ul/li[2]/a"));
+        homeFromMenuInLoginPage.click();
+        Assert.assertEquals("https://katalon-demo-cura.herokuapp.com/", driver.getCurrentUrl());
+    }
 
+    public void returnToHomepageFromAppointmentFormPage() {
+        driver.get(url);
+        WebElement makeAppointmentButton = driver.findElement(By.id("btn-make-appointment"));
+        makeAppointmentButton.click();
+        WebElement inputUsername = driver.findElement(By.id("txt-username"));
+        inputUsername.click();
+        inputUsername.sendKeys("John Doe");
+        WebElement inputPassword = driver.findElement(By.id("txt-password"));
+        inputPassword.click();
+        inputPassword.sendKeys("ThisIsNotAPassword");
+        WebElement loginSubmitButton = driver.findElement(By.id("btn-login"));
+        loginSubmitButton.click();
+        WebElement menuButton = driver.findElement(By.id("menu-toggle"));
+        menuButton.click();
+        WebElement homeFromMenuInAppointmentFormPage = driver.findElement(By.xpath("//*[@id='sidebar-wrapper']/ul/li[2]/a"));
+        homeFromMenuInAppointmentFormPage.click();
+        Assert.assertEquals("https://katalon-demo-cura.herokuapp.com/", driver.getCurrentUrl());
+    }
+
+    public void returnToHomepageFromAppointmentConfirmationPage() {
+        driver.get(url);
+        WebElement makeAppointmentButton = driver.findElement(By.id("btn-make-appointment"));
+        makeAppointmentButton.click();
+        WebElement inputUsername = driver.findElement(By.id("txt-username"));
+        inputUsername.click();
+        inputUsername.sendKeys("John Doe");
+        WebElement inputPassword = driver.findElement(By.id("txt-password"));
+        inputPassword.click();
+        inputPassword.sendKeys("ThisIsNotAPassword");
+        WebElement loginSubmitButton = driver.findElement(By.id("btn-login"));
+        loginSubmitButton.click();
+        WebElement visitDate = driver.findElement(By.id("txt_visit_date"));
+        visitDate.sendKeys("1/03/2023");
+        WebElement comment = driver.findElement(By.id("txt_comment"));
+        comment.sendKeys("comment");
+        WebElement bookAppointmentButton = driver.findElement(By.id("btn-book-appointment"));
+        bookAppointmentButton.click();
+        WebElement goToHomepageButton = driver.findElement(By.xpath("//*[@id='summary']/div/div/div[7]/p/a"));
+        goToHomepageButton.click();
+        Assert.assertEquals("https://katalon-demo-cura.herokuapp.com/", driver.getCurrentUrl());
+    }
+    }
