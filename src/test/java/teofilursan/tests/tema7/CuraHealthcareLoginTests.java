fdb4105567720a27bf80521ce3e91007b0b66101
Teo <ursan.teofil@yahoo.com>
2023-01-17 19:40:11 +0200

    Al doilea scenariu de login

--- src/test/java/teofilursan/tests/tema7/CuraHealthcareLoginTests.java
+++ src/test/java/teofilursan/tests/tema7/CuraHealthcareLoginTests.java
@@ -27,4 +27,26 @@ public class CuraHealthcareLoginTests extends BaseTest {
                 "https://katalon-demo-cura.herokuapp.com/#appointment",
                 "Couldn't navigate to appointment page!");
     }
+
+    @Test
+    public void loginWithInvalidCredentials() {
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+        WebElement menu = driver.findElement(By.cssSelector(".btn-dark.btn-lg.toggle"));
+        menu.click();
+        WebElement loginLink = driver.findElement(By.linkText("Login"));
+        loginLink.click();
+        Assert.assertEquals(driver.getCurrentUrl(),
+                "https://katalon-demo-cura.herokuapp.com/profile.php#login",
+                "Couldn't navigate to login page!");
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        usernameInput.sendKeys("John Do");
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        passwordInput.sendKeys("ThisIsNotAPassword");
+        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginButton.click();
+        WebElement unsuccessfulLoginMessage = driver.findElement(By.className("text-danger"));
+        Assert.assertEquals(unsuccessfulLoginMessage.getText(),
+                "Login failed! Please ensure the username and password are valid.",
+                "Shouldn't navigate to appointment page!");
+    }
 }
