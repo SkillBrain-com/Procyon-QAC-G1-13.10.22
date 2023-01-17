1f01e68ecfa1413c658200889a0de54b0b09b8a2
tincageorgianaalina <tincageorgianaalina@gmail.com>
2023-01-17 11:51:09 +0000

    Am adaugat teoria de la TestNG

--- src/test/java/AlinaTinca/Teorie/DemoQaInputs.java
+++ src/test/java/AlinaTinca/Teorie/DemoQaInputs.java
@@ -9,7 +9,7 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import teofilursan.driver.BrowserManager;
+import alinatinca.driver.BrowserManager;
 
 @Test
 public class DemoQaInputs {
