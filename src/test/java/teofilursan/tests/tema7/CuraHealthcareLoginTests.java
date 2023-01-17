f55492816a9aa3c702364c5c2605e887416271e5
Teo <ursan.teofil@yahoo.com>
2023-01-17 20:14:02 +0200

    Added dataProvider

--- src/test/java/teofilursan/tests/tema7/CuraHealthcareLoginTests.java
+++ src/test/java/teofilursan/tests/tema7/CuraHealthcareLoginTests.java
@@ -3,33 +3,21 @@ package teofilursan.tests.tema7;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CuraHealthcareLoginTests extends BaseTest {
 
-    @Test
-    public void loginWithValidCredentials() {
-        driver.get("https://katalon-demo-cura.herokuapp.com/");
-        WebElement menu = driver.findElement(By.cssSelector(".btn-dark.btn-lg.toggle"));
-        menu.click();
-        WebElement loginLink = driver.findElement(By.linkText("Login"));
-        loginLink.click();
-        Assert.assertEquals(driver.getCurrentUrl(),
-                "https://katalon-demo-cura.herokuapp.com/profile.php#login",
-                "Couldn't navigate to login page!");
-        WebElement usernameInput = driver.findElement(By.name("username"));
-        usernameInput.sendKeys("John Doe");
-        WebElement passwordInput = driver.findElement(By.name("password"));
-        passwordInput.sendKeys("ThisIsNotAPassword");
-        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
-        loginButton.click();
-        Assert.assertEquals(driver.getCurrentUrl(),
-                "https://katalon-demo-cura.herokuapp.com/#appointment",
-                "Couldn't navigate to appointment page!");
+    @DataProvider(name = "loginDataProvider")
+    public Object[][] loginDataProvider() {
+        return new Object[][]{
+                {"John Doe", "ThisIsNotAPassword", "validCredentials"},
+                {"John Do", "ThisIsNotAPassword", "invalidCredentials"}
+        };
     }
 
-    @Test
-    public void loginWithInvalidCredentials() {
+    @Test(dataProvider = "loginDataProvider")
+    public void loginWithCredentials(String username, String password, String credentialsType) {
         driver.get("https://katalon-demo-cura.herokuapp.com/");
         WebElement menu = driver.findElement(By.cssSelector(".btn-dark.btn-lg.toggle"));
         menu.click();
@@ -39,14 +27,20 @@ public class CuraHealthcareLoginTests extends BaseTest {
                 "https://katalon-demo-cura.herokuapp.com/profile.php#login",
                 "Couldn't navigate to login page!");
         WebElement usernameInput = driver.findElement(By.name("username"));
-        usernameInput.sendKeys("John Do");
+        usernameInput.sendKeys(username);
         WebElement passwordInput = driver.findElement(By.name("password"));
-        passwordInput.sendKeys("ThisIsNotAPassword");
+        passwordInput.sendKeys(password);
         WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
         loginButton.click();
-        WebElement unsuccessfulLoginMessage = driver.findElement(By.className("text-danger"));
-        Assert.assertEquals(unsuccessfulLoginMessage.getText(),
-                "Login failed! Please ensure the username and password are valid.",
-                "Shouldn't navigate to appointment page!");
+        if (credentialsType.equals("validCredentials")) {
+            Assert.assertEquals(driver.getCurrentUrl(),
+                    "https://katalon-demo-cura.herokuapp.com/#appointment",
+                    "Couldn't navigate to appointment page!");
+        } else {
+            WebElement unsuccessfulLoginMessage = driver.findElement(By.className("text-danger"));
+            Assert.assertEquals(unsuccessfulLoginMessage.getText(),
+                    "Login failed! Please ensure the username and password are valid.",
+                    "Shouldn't navigate to appointment page!");
+        }
     }
 }
