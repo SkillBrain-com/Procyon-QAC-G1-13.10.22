f47e4d9861c5a672c2b4781790d9c7265400e7cc
tincageorgianaalina <tincageorgianaalina@gmail.com>
2023-01-17 10:39:08 +0000

    Am creat clasa de BrowserManager

---
+++ src/main/java/alinatinca/driver/BrowserManager.java
@@ -0,0 +1,13 @@
+package alinatinca.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserManager {
+
+    public static ChromeDriver createChromeDriver(){
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+}
