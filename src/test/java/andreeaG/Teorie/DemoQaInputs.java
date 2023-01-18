eb564450ecfa405e1bd4b3c1107b4c2e469cdd04
redandreea <redandreea@gmail.com>
2023-01-18 16:54:41 +0200

    teorie, reluat dupa recording

--- src/test/java/andreeaG/Teorie/DemoQaInputs.java
+++ src/test/java/andreeaG/Teorie/DemoQaInputs.java
@@ -1,48 +1,57 @@
-package AndreeaG.Teorie;
+package andreeaG.Teorie;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import teofilursan.driver.BrowserManager;
 
-public class Demoqa {
+
+
+public class DemoQaInputs {
 
     ChromeDriver driver;
-    @Test
+@Test
     public void test1(){
         driver= BrowserManager.createChromeDriver();
         driver.get("https://demoqa.com/text-box");
         writeFullName();
         writeEmail();
-        writeCurrentAddy();
-        writePermanentAddy();
-
+        writeCurrentAddress();
+        writePermanentAddress();
         driver.quit();
-
     }
-
-
     public void writeFullName() {
-
         WebElement fullNameInput = driver.findElement(By.id("userName"));
         fullNameInput.sendKeys("Teofil Ursan");
     }
-
     public void writeEmail() {
-
         WebElement emailInput = driver.findElement(By.id("userEmail"));
         emailInput.sendKeys("teo@test.com");
     }
-
-
     public void writeCurrentAddress() {
         WebElement currentAddressInput = driver.findElement(By.id("currentAddress"));
         currentAddressInput.sendKeys("Iasi");
     }
-
-    public void writePermanentAddy() {
+    public void writePermanentAddress() {
         WebElement permanentAddressInput = driver.findElement(By.id("permanentAddress"));
         permanentAddressInput.sendKeys("Iasi");
+    }
+    public void clickOnSubmitButton() {
+        WebElement submitButton = driver.findElement(By.id("submit"));
+//        Actions actions = new Actions(driver);
+//        actions.moveToElement(submitButton).build().perform();
+//        submitButton.click();
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", submitButton);
+    }
+
+    public void verifySubmittedDetails() {
+        WebElement nameParagraph = driver.findElement(By.id("name"));
+        Assert.assertEquals(nameParagraph.getText(), "Name:Teofil Ursan", "Values are different for name!");
+        WebElement emailParagraph = driver.findElement(By.id("email"));
+        Assert.assertTrue(emailParagraph.getText().equals("Email:teo@test.com"), "Email is not correct!");
     }
 }
---
+++ src/test/java/andreeaG/Teorie/altpachet/AltTest.java
@@ -0,0 +1,13 @@
+package andreeaG.Teorie.altpachet;
+
+import org.testng.annotations.Test;
+
+public class AltTest {
+
+        @Test
+        public void writeMessage() {
+            System.out.println("mesajul meu din AltTest Class");
+        }
+
+    }
+
