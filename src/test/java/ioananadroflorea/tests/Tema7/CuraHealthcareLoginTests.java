7d1ebe046c5fe0ed61349f9f3d18cf4643cf8cd8
Ioana Andronache-Florea <ioana.florea88@yahoo.com>
2023-01-19 19:15:45 +0200

    Mici modificari

--- src/test/java/ioananadroflorea/tests/Tema7/CuraHealthcareLoginTests.java
+++ src/test/java/ioananadroflorea/tests/Tema7/CuraHealthcareLoginTests.java
@@ -16,47 +16,39 @@ public class CuraHealthcareLoginTests extends BaseTest {
         };
 
     }
-
-    @Test(dataProvider = "loginDataProvider")
-    public void loginWithValidCredentials(String username, String password, String credentialsType){
-        driver.get("https://katalon-demo-cura.herokuapp.com/");
+    @Test(dataProvider = "loginDataProvider", groups = "login")
+    public void loginWithCredentials(String username, String password, String credentialsType) {
         WebElement menu = driver.findElement(By.cssSelector(".btn-dark.btn-lg.toggle"));
         menu.click();
         WebElement loginLink = driver.findElement(By.linkText("Login"));
         loginLink.click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://katalon-demo-cura.herokuapp.com/profile.php#login", "Couldn't navigate to login page!");
-        WebElement usernameInput = driver.findElement(By.name("username"));
-        usernameInput.sendKeys("John Doe");
-        WebElement passwordInput = driver.findElement(By.name("password"));
-        passwordInput.sendKeys("ThisIsNotAPassword");
-        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
-        loginButton.click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://katalon-demo-cura.herokuapp.com/#appointment","Couldn't navigate to appointment page!");
-    }
-
-    @Test
-    public void loginWitInvalidCredentials(){
-        driver.get("https://katalon-demo-cura.herokuapp.com/");
-        WebElement menu = driver.findElement(By.cssSelector(".btn-dark.btn-lg.toggle"));
-        menu.click();
-        WebElement loginLink = driver.findElement(By.linkText("Login"));
-        loginLink.click();
-        Assert.assertEquals(driver.getCurrentUrl(), "https://katalon-demo-cura.herokuapp.com/profile.php#login", "Couldn't navigate to login page!");
-        WebElement usernameInput = driver.findElement(By.name("username"));
-        usernameInput.sendKeys("John Do");
-        WebElement passwordInput = driver.findElement(By.name("password"));
-        passwordInput.sendKeys("ThisIsNotAPassword");
-        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
-        loginButton.click();
-        WebElement unsuccessfullLoginMessage = driver.findElement(By.className("text-danger"));
-        Assert.assertEquals(unsuccessfullLoginMessage.getText(), "Login failed! Please ensure the username and password are valid.","Shouldn't navigate to appointment page!");
-    }
-    @Test
-    public void verifyRedirectToLoginWhenUserNotLoggedIn(){
-        driver.get("https://katalon-demo-cura.herokuapp.com/");
-        WebElement makeAppointment = driver.findElement(By.id("btn-make-appointment"));
-        makeAppointment.click();
         Assert.assertEquals(driver.getCurrentUrl(),
-                "");
+                "https://katalon-demo-cura.herokuapp.com/profile.php#login",
+                "Couldn't navigate to login page!");
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        usernameInput.sendKeys(username);
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        passwordInput.sendKeys(password);
+        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginButton.click();
+        if (credentialsType.equals("validCredentials")) {
+            Assert.assertEquals(driver.getCurrentUrl(),
+                    "https://katalon-demo-cura.herokuapp.com/#appointment",
+                    "Couldn't navigate to appointment page!");
+        } else {
+            WebElement unsuccessfulLoginMessage = driver.findElement(By.className("text-danger"));
+            Assert.assertEquals(unsuccessfulLoginMessage.getText(),
+                    "Login failed! Please ensure the username and password are valid.",
+                    "Shouldn't navigate to appointment page!");
+        }
+    }
+
+    @Test(groups = "redirect")
+    public void verifyRedirectToLoginWhenUserNotLoggedIn() {
+        WebElement makeAppointmentButton = driver.findElement(By.id("btn-make-appointment"));
+        makeAppointmentButton.click();
+        Assert.assertEquals(driver.getCurrentUrl(),
+                "https://katalon-demo-cura.herokuapp.com/profile.php#login",
+                "User is not redirected to login page when not logged in!");
     }
 }
