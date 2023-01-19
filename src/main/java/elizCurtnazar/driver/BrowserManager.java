9c66cc5caf72c8373ade47f5c6fa9f1851977044
Curtnazar Eliz <eliz_rts_91@hotmail.com>
2023-01-19 21:41:49 +0200

    selenium 3

--- src/main/java/elizCurtnazar/driver/BrowserManager.java
+++ src/main/java/elizCurtnazar/driver/BrowserManager.java
@@ -2,11 +2,38 @@ package elizCurtnazar.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BrowserManager {
     public static ChromeDriver createDriver(){ //cream un obiect de tipu driver. cream pagina de chrome. o inscriem intro clasa
         WebDriverManager.chromedriver().setup(); // initializam
         return new ChromeDriver(); // returnam o instanta de chromedriver
+    }
+
+
+    //nu
+    public static ChromeDriver createChromeDriverWithOptions() {
+        //creating a ChromeOptions object
+        ChromeOptions options = new ChromeOptions();
+        //set fullscreen option
+        options.addArguments("start-maximized");
+        //initialise driver with options defined above
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver(options);
+    }
+
+    public static ChromeDriver createChromeDriverForMobile(){
+        Map<String, String> mobileEmulation = new HashMap<>();
+//        "Vulpe" -> "Rosie"
+        mobileEmulation.put("deviceName", "Galaxy S8");
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);//options.addArguments
+//        WebDriverManager.chromedriver().setup(); //in caz de erroare
+        return new ChromeDriver(chromeOptions);
     }
 
     public static void closeChromeDriver(ChromeDriver driver){
