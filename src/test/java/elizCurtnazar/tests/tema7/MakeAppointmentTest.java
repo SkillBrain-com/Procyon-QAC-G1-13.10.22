e29462959c3687402cebfecdfd30fd70370f488c
Curtnazar Eliz <eliz_rts_91@hotmail.com>
2023-01-19 19:20:23 +0200

    commit

---
+++ src/test/java/elizCurtnazar/tests/tema7/MakeAppointmentTest.java
@@ -0,0 +1,13 @@
+package elizCurtnazar.tests.tema7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class MakeAppointmentTest extends BaseTest{
+
+    @Test
+    public void clickMakeAppointmentButton() {
+        WebElement makeAppointmentButton = driver.findElement(By.id(""));
+    }
+}
