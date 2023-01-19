24ec6d7eb054ea40b00eb1060d09aa177f637665
SanduBogdan95 <bogdan95.sandu@gmail.com>
2023-01-19 17:51:15 +0200

    Corectat

--- src/main/java/bogdanSandu/driver/BrowserManager.java
+++ src/main/java/bogdanSandu/driver/BrowserManager.java
@@ -1,4 +1,4 @@
-package BogdanSandu.driver;
+package bogdanSandu.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
--- src/main/java/bogdanSandu/utils/FileUtils.java
+++ src/main/java/bogdanSandu/utils/FileUtils.java
@@ -1,4 +1,4 @@
-package BogdanSandu.utils;
+package bogdanSandu.utils;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeDriver;
