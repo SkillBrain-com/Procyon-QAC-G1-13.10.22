eb88311028bb6ec227bd8b7df8a26c5ca45e40b8
Teo <ursan.teofil@yahoo.com>
2023-01-19 21:44:36 +0200

    Added course 8 example

---
+++ src/test/java/teofilursan/teorie/seleniumbasics3/pageObjects/EmagHomePage.java
@@ -0,0 +1,25 @@
+package teofilursan.teorie.seleniumbasics3.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EmagHomePage {
+
+    ChromeDriver driver;
+
+    public EmagHomePage(ChromeDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    //WebElement acceptButton = driver.findElement(By.className("js-accept"));
+    @FindBy(className = "js-accept")
+    WebElement acceptButton;
+
+    public void clickOnAcceptButton() {
+        acceptButton.click();
+    }
+}
