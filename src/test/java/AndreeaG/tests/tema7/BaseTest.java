d9f8a74f448debf137a1bf7cdcb0441c72e0e5f2
redandreea <redandreea@gmail.com>
2023-01-22 13:25:50 +0200

    am lucrat la tema 7

--- src/test/java/AndreeaG/tests/tema7/BaseTest.java
+++ src/test/java/AndreeaG/tests/tema7/BaseTest.java
@@ -2,22 +2,24 @@ package AndreeaG.tests.tema7;
 
 import AndreeaG.driver.BrowserManager;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
     protected ChromeDriver driver;
 
-    @BeforeMethod
-    public void initChromeBrowser(){
-       driver = BrowserManager.createChromeDriver();
+    @BeforeMethod(groups = "redirect")
+    public void initChromeBrowser() {
+        driver = BrowserManager.createChromeDriver();
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+        System.out.println("Executed before method");
     }
-    @AfterMethod
-    public void quitDriver(){
-        if (driver!=null){
+
+   /* @AfterMethod(groups = "redirect")
+    public void quitDriver() {
+        if (driver!=null) {
             driver.quit();
         }
-        System.out.println ("Am executat");
-    }
+        System.out.println("Executed after method");
+    }*/
 }
