c2575655a9b3ba8b829fd78ea7d45c754a7c972e
tincageorgianaalina <tincageorgianaalina@gmail.com>
2023-01-23 20:55:04 +0000

    se continua

--- src/test/java/alinaTinca/tests/Tema8/pageObjects/HerokuBasicAjaxExamplePage.java
+++ src/test/java/alinaTinca/tests/Tema8/pageObjects/HerokuBasicAjaxExamplePage.java
@@ -1,53 +1,68 @@
-package alinaTinca.tests.Tema8.pageObjects;
+package alinatinca.tests.Tema8.pageObjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.*;
 
 import java.awt.*;
+import java.time.Duration;
 
 public class HerokuBasicAjaxExamplePage {
 
+//0. Creezi atributele:
     ChromeDriver driver;
 
+    WebDriverWait wait;
+
+    Wait<WebDriver> fluentWait;
+
+//1. Creezi constructorul
     public HerokuBasicAjaxExamplePage(ChromeDriver driver){
         this.driver = driver;
-//        PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        fluentWait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(1))
+                        .pollingEvery(Duration.ofMillis(200));
+        PageFactory.initElements(driver, this); //identifica elementele pe pagina
     }
 
 
+//2. Identificam elementele de pe pagina cu FindBy
 
     //pentru Categoriile de selectat
     @FindBy(id = "combo1")
-    String chooseCategoryItem;
+    WebElement category;
 
-//    //incomplet
-//    public void selectOption(String option) {
-//        Select select = new Select(driver.findElement(By.id("combo1")));
-//        select.selectByVisibleText(option);
-//    }
 
+    //FindBy pt Language
+    @FindBy (id = "combo2")
+    WebElement language;
 
     //pentru butonul CODEINIT
     @FindBy(className = "styled-click-button")
     WebElement codeInItButton;
 
+
+    //2. Definim metodele
+    public void selectCategory(String categoryName){
+        Select category = new Select(this.category);
+        category.selectByVisibleText(categoryName);
+    }
+
+    public void selectLanguage(String languageName){
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("ajaxBusy")));
+        fluentWait.until(ExpectedCondition.invisibilityOfElementLocated(By.id("ajaxBusy")));
+        Select language = new Select(this.language);
+        language.selectByVisibleText(languageName);
+    }
+
     public void clickOnCodeInIt(){
         codeInItButton.click();
         System.out.println("Code In It button has been clicked!");
-    }
-
-
-    //pentru butonul GoToTheFormButton
-    @FindBy(className = "styled-click-button")
-    WebElement goBackToTheFormButton;
-
-    public void clickOnGoToTheFormButton(){
-        goBackToTheFormButton.click();
-        System.out.println("Return on Basic Ajax page!");
     }
 
 }
--- src/test/java/alinaTinca/tests/Tema8/tests/HerokuBasicAjaxExamplePageTests.java
+++ src/test/java/alinaTinca/tests/Tema8/tests/HerokuBasicAjaxExamplePageTests.java
@@ -1,6 +1,6 @@
-package alinaTinca.tests.Tema8.tests;
+package alinatinca.tests.Tema8.tests;
 
-import alinaTinca.tests.Tema8.pageObjects.HerokuBasicAjaxExamplePage;
+import alinatinca.tests.Tema8.pageObjects.HerokuBasicAjaxExamplePage;
 import alinatinca.driver.BrowserManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -22,35 +22,45 @@ public class HerokuBasicAjaxExamplePageTests {
         System.out.println(paragraph.getText());
     }
 
-    @Test(groups = "Test for desktop")
+    @Test(groups = {"mobile", "desktop"})
     public void getWebCategoryButtonForDesktop(){
         driver = BrowserManager.createChromeDriverWithOptions();
         driver.get("https://testpages.herokuapp.com/styled/basic-ajax-test.html");
         System.out.println("Heroku Basic Ajax Example page has been opened on desktop!");
 
-        Select chooseCategory = new Select(driver.findElement(By.id("combo1")));
-        chooseCategory.selectByValue("1");
+//        Select chooseCategory = new Select(driver.findElement(By.id("combo1")));
+//        chooseCategory.selectByValue("1");
 
-        System.out.println("Web item has been selected!");
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(7));
-        System.out.println("The 7 minutes have passed!");
-
-        WebElement codeInItButton = driver.findElement(By.className("styled-click-button"));
-        codeInItButton.click();
+        HerokuBasicAjaxExamplePage herokuBasicAjaxExamplePage = new HerokuBasicAjaxExamplePage(driver);
+        herokuBasicAjaxExamplePage.selectCategory("Web");
+        System.out.println("Web item from drop down category list has been selected!");
+        herokuBasicAjaxExamplePage.selectLanguage("Javascript");
+        System.out.println("Javascript item from drop down language list has been selected!");
+        herokuBasicAjaxExamplePage.clickOnCodeInIt();
         System.out.println("Code In It button has been clicked!");
 
-        Assert.assertEquals(driver.getCurrentUrl(),
-                "https://testpages.herokuapp.com/styled/the_form_processor.php?ajax=1",
-                "The informations about Web item has been displayed in the URL above with the following body:");
-        System.out.println(driver.findElement(By.className("page-body")).getText());
 
-        WebElement goBackToTheFormButton = driver.findElement(By.className("styled-click-button"));
-        goBackToTheFormButton.click();
 
-        driver.quit();
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(7));
+//        System.out.println("The 7 minutes have passed!");
+//
+////        WebElement codeInItButton = driver.findElement(By.className("styled-click-button"));
+////        codeInItButton.click();
+////        System.out.println("Code In It button has been clicked!");
+//
+//        Assert.assertEquals(driver.getCurrentUrl(),
+//                "https://testpages.herokuapp.com/styled/the_form_processor.php?ajax=1",
+//                "The informations about Web item has been displayed in the URL above with the following body:");
+//        System.out.println(driver.findElement(By.className("page-body")).getText());
+//
+//        WebElement goBackToTheFormButton = driver.findElement(By.className("styled-click-button"));
+//        goBackToTheFormButton.click();
+//
+//        driver.quit();
     }
 
+// In metoda: apelam constructorul apoi fiecare metoda in parte si dam parametrii noi (cele 3 metode)
+    //Assert -. in care se defineste intro clasa separata in pageObject ... Basic..ConfirmatinPage
 
     @Test(groups = "Test for mobile")
     public void getWebCategoryButtonForMobile(){
