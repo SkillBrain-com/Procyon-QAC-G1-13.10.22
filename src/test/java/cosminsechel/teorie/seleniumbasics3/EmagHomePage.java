a1e5f0d3d5b5e51a852f4f532c61d1e0c2f82493
cosmin89 <cosmin.sechel89@gmail.com>
2023-01-26 20:52:26 +0200

    selenium grid teorie

--- src/test/java/cosminsechel/teorie/seleniumbasics3/EmagHomePage.java
+++ src/test/java/cosminsechel/teorie/seleniumbasics3/EmagHomePage.java
@@ -3,22 +3,28 @@ package cosminsechel.teorie.seleniumbasics3;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.PageFactoryFinder;
 
 public class EmagHomePage {
 
-    ChromeDriver driver;
-    public EmagHomePage(ChromeDriver driver) {
-        this.driver = driver;
+    ChromeDriver chromeDriver;
+    RemoteWebDriver remoteWebDriver;
+
+    public EmagHomePage(RemoteWebDriver driver) {
+        this.remoteWebDriver = driver;
         PageFactory.initElements(driver, this);
     }
+    public EmagHomePage(ChromeDriver driver) {
+        this.chromeDriver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    //WebElement acceptButton = driver.findElement(By.className("js-accept"));
     @FindBy(className = "js-accept")
     WebElement acceptButton;
 
     public void clickOnAcceptButton() {
         acceptButton.click();
     }
-
 }
--- src/test/java/cosminsechel/teorie/seleniumbasics3/EmagTests.java
+++ src/test/java/cosminsechel/teorie/seleniumbasics3/EmagTests.java
@@ -1,52 +1,55 @@
 package cosminsechel.teorie.seleniumbasics3;
 
-import cosminsechel.driver.BrowserManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import cosminsechel.driver.BrowserManager;
+import cosminsechel.teorie.seleniumbasics3.EmagHomePage;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.temporal.TemporalUnit;
-import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
 
 public class EmagTests {
 
-    ChromeDriver driver;
+    //ChromeDriver driver;
+    RemoteWebDriver driver;
     @Test
     public void openLoginTest() {
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        driver = BrowserManager.createChromeDriverWithOptions();
+        //driver = BrowserManager.createChromeDriverWithOptions();
+        driver = BrowserManager.createRemoteWebDriver();
         driver.get("https://www.emag.ro/");
-        System.out.println(LocalDateTime.now());
+        System.out.println("Inainte " + LocalDateTime.now());
         //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        //WebElement acceptButton = new WebDriverWait(driver, Duration.ofSeconds(5))
-                //.until(ExpectedConditions.elementToBeClickable(By.className("js-accept")));
-        WebElement acceptButton = driver.findElement(By.className("js-accept"));
-        //acceptButton.click();
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click();", acceptButton);
+//        WebElement acceptButton = new WebDriverWait(driver, Duration.ofSeconds(5))
+//                .until(ExpectedConditions
+//                        .elementToBeClickable(By.className("js-accept")));
         Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
                 .withTimeout(Duration.ofSeconds(30))
                 .pollingEvery(Duration.ofSeconds(5))
                 .ignoring(NoSuchElementException.class);
         wait.until(ExpectedConditions
                 .visibilityOfElementLocated(By.className("js-accept")));
+        System.out.println("Dupa " + LocalDateTime.now());
+        WebElement acceptButton = driver.findElement(By.className("js-accept"));
+        //acceptButton.click();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", acceptButton);
         driver.quit();
-
     }
+
+    @Test
+    public void openLoginTestWithPageObject() {
+        driver = BrowserManager.createChromeDriverWithOptions();
+        driver.get("https://www.emag.ro/");
+        EmagHomePage emagHomePage = new EmagHomePage(driver);
+        emagHomePage.clickOnAcceptButton();
+        driver.quit();
+    }
+
     @Test
     public void openMobileBrowser() {
         driver = BrowserManager.createChromeDriverForMobile();
