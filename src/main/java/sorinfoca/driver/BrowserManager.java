f2fbe00bf3afcbde9725e53e213e9d7bcdcbc0d1
Fsorin <sorinfoca@yahoo.com>
2023-01-26 21:16:06 +0200

    Tema 9 Completa

--- src/main/java/sorinfoca/driver/BrowserManager.java
+++ src/main/java/sorinfoca/driver/BrowserManager.java
@@ -2,10 +2,27 @@ package sorinfoca.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BrowserManager {
     public static ChromeDriver createChromeDriver() {
         WebDriverManager.chromedriver().setup();
         return new ChromeDriver();
+    }
+
+    RemoteWebDriver driver;
+
+    public static RemoteWebDriver createRemoteWebDriver() {
+
+        try {
+            return new RemoteWebDriver(new URL("http://localhost:4444/"),new ChromeOptions());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
