e109c34229b1c659bd280651d00ade80bcfdc857
claudiu357 <neculaclaudiu21@gmail.com>
2023-01-27 08:10:57 -0800

    am completat toata tema 8

--- src/test/java/neculaclaudiu/tests/seleniumpart3/tests/BaseTest.java
+++ src/test/java/neculaclaudiu/tests/seleniumpart3/tests/BaseTest.java
@@ -4,14 +4,20 @@ import neculaclaudiu.driver.ChromeDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 
 public class BaseTest {
 
     protected ChromeDriver driver;
 
-    @BeforeMethod
+    @BeforeMethod(groups = "Desktop")
     public void initChromeBrowser(){
         driver = ChromeDriverManager.createChromeDriverWithOptions();
+    }
+
+    @BeforeMethod(groups = "Mobile")
+    public void initMobileChromeBrowser(){
+        driver = ChromeDriverManager.createChromeDriverForMobile();
     }
 
     @AfterMethod(alwaysRun = true)
