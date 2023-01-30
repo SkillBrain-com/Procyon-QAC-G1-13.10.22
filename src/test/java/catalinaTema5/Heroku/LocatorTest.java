b749a5fa199c11f179fcb9c6e8bebb6c5d970a11
zoldicatalina <117173529+zoldicatalina@users.noreply.github.com>
2023-01-30 16:07:22 +0200

    Update LocatorTest.java

--- src/test/java/catalinaTema5/Heroku/LocatorTest.java
+++ src/test/java/catalinaTema5/Heroku/LocatorTest.java
@@ -8,19 +8,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
      public class  LocatorTest {
 
-         public static void main(String[] args) {
-
-             LocatorTest();
-    }
-
-
-
-        public static void LocatorTest() {
+        
+       public static void LocatorTest() {
         ChromeDriver driver = BrowserManager.createChromeDriver();
         driver.get("https://testpages.herokuapp.com/styled/find-by-playground-test.html");
 
         WebElement firstPharagraph = driver.findElement(By.className("explanation"));
         System.out.println("Primul pharagraf are textul:" + firstPharagraph.getText());
+        WebElement indexButton = driver.findElement(By.xPath("/html/body/div[1]/div[1]/a"));
+        indexButton.get.click;
         driver.quit();
 
     }
