6f01442c37434456a231ec3bd75a8c261fb7b92b
zoldicatalina <117173529+zoldicatalina@users.noreply.github.com>
2023-01-30 15:14:47 +0200

    Update RadioButtons.java

--- src/test/java/catalinaTema5/DemoqaElements/RadioButtons.java
+++ src/test/java/catalinaTema5/DemoqaElements/RadioButtons.java
@@ -8,12 +8,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class RadioButtons {
     public static void main(String[] args){
         goToRadioButtonsPage();
-
-    }
+ }
 
     public static  void goToRadioButtonsPage() {
      ChromeDriver driver = BrowserManager.createChromeDriver();
-     driver.get("https://demoqa.com/elements");
+     driver.get("https://demoqa.com/radio-button");
         WebElement radioButtonNo = driver.findElement(By.xpath("//label[@class='custom-control-label disable' and @for='noRadio']"));
         radioButtonNo.click();
         WebElement radioButtonImpressive =driver.findElement(By.xpath("//label[@class='custom-control-label' and @for='impressiveRadio']"));
