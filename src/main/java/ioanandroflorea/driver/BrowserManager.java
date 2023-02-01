837d77f2b4ef2c16fb87cc7171f8cb10a1b5d772
Ioana Andronache-Florea <ioana.florea88@yahoo.com>
2023-02-01 17:18:01 +0200

    Tema 9 -inceput

--- src/main/java/ioanandroflorea/driver/BrowserManager.java
+++ src/main/java/ioanandroflorea/driver/BrowserManager.java
@@ -3,7 +3,10 @@ package ioanandroflorea.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +19,6 @@ public class BrowserManager {
     public static void closeBrowser(ChromeDriver driver) {
         driver.quit();
     }
-
     public static ChromeDriver createChromeDriverForDesktop() {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("start-maximized");
@@ -30,4 +32,26 @@ public class BrowserManager {
             chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
             return new ChromeDriver(chromeOptions);
     }
+    public static RemoteWebDriver createRemoteWebDriver(){
+        try {
+            return new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        public static void closeBrowser(RemoteWebDriver driver) {
+            driver.quit();
+        }
+
+        public static RemoteWebDriver createRemoteWebDriverForDesktop() {
+            RemoteWebDriverOptions options = new RemoteWebDriverOptions();
+            options.addArguments("start-maximized");
+            WebDriverManager.chromedriver().setup();
+            return new RemoteWebDriver(options);
+        }
+        public static RemoteWebDriver createRemoteWebDriverForMobile(){
+            Map<String, String> mobileEmulation = new HashMap<>();
+            mobileEmulation.put("deviceName", "Galaxy S8");
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+            return new RemoteWebDriver(chromeOptions);
 }
