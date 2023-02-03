f5cc76fb5a44302384b2ac764b190f07f6a680ab
tincageorgianaalina <tincageorgianaalina@gmail.com>
2023-02-03 16:06:18 +0000

    actualizat

--- src/test/java/alinaTinca/teorie/ProiectTimeea/Heroku/AlertsTests.java
+++ src/test/java/alinaTinca/teorie/ProiectTimeea/Heroku/AlertsTests.java
@@ -1,18 +1,17 @@
 package alinatinca.teorie.ProiectTimeea.Heroku;
 
 import AlinaTinca.driver.BrowserManager;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class AlertsTests {
 
     public static void main(String[] args) {
-        secondAlert();
+//        secondAlert();
+        thirdAlert();
     }
 
     public static void firstAlert(){
@@ -70,6 +69,45 @@ public class AlertsTests {
             return true;
         }catch(NoAlertPresentException e){
             return false;
+        }
+    }
+
+    public static void thirdAlert(){
+        ChromeDriver driver = null;
+        try{
+            driver = BrowserManager.createChromeDriver();
+            driver.get("https://testpages.herokuapp.com/styled/alerts/alert-test.html");
+            System.out.println("Navigheaza la pagina" +
+                    "");
+            WebElement thirdButton = driver.findElement(By.id("promptexample"));
+            Actions actions = new Actions(driver);
+            actions.moveToElement(thirdButton).build().perform();
+            System.out.println("Face scroll pana la al treilea buton de pe pagina");
+            thirdButton.click();
+            System.out.println("Da clic pe al treilea buton");
+
+            Alert thirdAlert1 = driver.switchTo().alert();
+            System.out.println("Indentifica alerta");
+            thirdAlert1.sendKeys("text for test");
+//            thirdAlert1.accept();
+//            System.out.println("Accepta alerta");
+//            WebElement messageAfterAcceptAlert1 = driver.findElement(By.id("promptreturn"));
+//            System.out.println("Mesajul dupa ce am introdus textul in aceasta alerta este: " + messageAfterAcceptAlert1.getText());
+//            WebElement messageForAcceptAlert1 = driver.findElement(By.id("promptexplanation"));
+//            System.out.println("Mesajul dupa ce alerta este acceptata este: " + messageForAcceptAlert1.getText());
+
+            thirdAlert1.dismiss();
+            WebElement messageAfterAcceptAlert2 = driver.findElement(By.id("promptreturn"));
+            System.out.println("Mesajul dupa ce am introdus textul in aceasta alerta este: " + messageAfterAcceptAlert2.getText());
+            WebElement messageForAcceptAlert2 = driver.findElement(By.id("promptexplanation"));
+            System.out.println("Textul dupa ce alerta este refuzata este: " + messageForAcceptAlert2.getText());
+        }catch (NoAlertPresentException e){
+            System.out.println("Exceptia nu mai apare!");
+        }
+        finally {
+            if (driver !=null ){
+                driver.quit();
+            }
         }
     }
 }
--- src/test/java/alinaTinca/teorie/ProiectTimeea/Heroku/FormsTests.java
+++ src/test/java/alinaTinca/teorie/ProiectTimeea/Heroku/FormsTests.java
@@ -17,13 +17,15 @@ public class FormsTests {
     public static void fillFormData() {
         ChromeDriver driver = BrowserManager.createChromeDriver();
         driver.get("https://testpages.herokuapp.com/styled/html5-form-test.html");
+        System.out.println("Navigheaza la pagina");
         WebElement paragraph = driver.findElement(By.className("explanation"));
         System.out.println("The message from the paragraph is: " + paragraph.getText());
         WebElement colour = driver.findElement(By.id("colour-picker"));
         colour.sendKeys("#861C06");
+        System.out.print("Introducem culoarea cu acest HEX: #861C06");
+        //Cum alegem culoarea?
         //JavascriptExecutor jse=(JavascriptExecutor)driver;
         //jse.executeScript("document.getElementByid('colour-picker').value='#861C06'");
-        // Cum alegem culoarea?
         WebElement data = driver.findElement(By.id("date-picker"));
         data.sendKeys("02/04/2023");
         WebElement localDataTime = driver.findElement(By.id("date-time-picker"));
@@ -31,15 +33,18 @@ public class FormsTests {
         //localDataTime.sendKeys("2016-03-03T02:02");
         JavascriptExecutor jse=(JavascriptExecutor)driver;
         jse.executeScript("document.getElementById('date-time-picker').value='2016-03-03T02:02'");
-        // Cum alegem ora?
+        //Cum alegem ora?
+        System.out.println("Introducem aceasta data: 2016-03-03T02:02");
         WebElement email = driver.findElement(By.id("email-field"));
         email.click();
         email.clear();
         email.sendKeys("test@test.com");
+        System.out.println("Introducem adresa de email: test@test.com");
         WebElement month = driver.findElement(By.id("month-field"));
         month.clear();
         month.sendKeys("July 2023");
-        // Cum alegem anul?
+        //Cum alegem anul?
+        System.out.println("Introducem aceasta data: July 2023");
         WebElement number = driver.findElement(By.id("number-field"));
         number.click();
         for (int i =0; i< 5; i++) {
