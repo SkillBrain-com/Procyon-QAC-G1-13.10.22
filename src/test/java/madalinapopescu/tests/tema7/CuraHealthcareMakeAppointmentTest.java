1a39418b3ceb84153fb92ea461068933d99283c1
pmadalina <pmadalina88@gmail.com>
2023-02-07 15:42:27 +0200

    Am facut push iar

--- src/test/java/madalinapopescu/tests/tema7/CuraHealthcareMakeAppointmentTest.java
+++ src/test/java/madalinapopescu/tests/tema7/CuraHealthcareMakeAppointmentTest.java
@@ -50,7 +50,7 @@ public class CuraHealthcareMakeAppointmentTest extends BaseTest{
         }
     }
 
-    @Testgit
+    @Test
     public void homeButton() {
         WebElement menuButton = driver.findElement(By.id("menu-toggle"));
         menuButton.click();
