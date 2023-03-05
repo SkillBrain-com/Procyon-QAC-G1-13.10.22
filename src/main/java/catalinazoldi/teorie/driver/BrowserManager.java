2c7a02f0454eecf01379aef6f447add386683383
zoldicatalina <zoldicatalina@yahoo.com>
2023-03-05 17:26:06 +0200

    Teorie capitolul9 facut la curs

--- src/main/java/catalinazoldi/teorie/driver/BrowserManager.java
+++ src/main/java/catalinazoldi/teorie/driver/BrowserManager.java
@@ -3,7 +3,10 @@ package catalinazoldi.teorie.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,5 +47,13 @@ public class BrowserManager {
         WebDriverManager.chromedriver().setup();
 
         return new ChromeDriver(chromeOptions);
+    }
+
+    public static RemoteWebDriver createRemoteWebDriver () {
+        try {
+            return  new RemoteWebDriver(new URL("http://localhost:4444/"),new ChromeOptions());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/catalinazoldi/teorie/SBpart3/tests/EmagTests.java
+++ src/test/java/catalinazoldi/teorie/SBpart3/tests/EmagTests.java
@@ -4,6 +4,7 @@ import catalinazoldi.teorie.driver.BrowserManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
@@ -13,22 +14,33 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class EmagTests {
-    ChromeDriver driver;
+
+
+    //ChromeDriver for SBpart3
+    // ChromeDriver driver;
+
+
+
+     //RemoteWebDriver for selenium grid
+    RemoteWebDriver driver;
 
     @Test
     public void openLoginTest() {
         //creaza obiectul ChromeOptions
-        ChromeOptions options = new ChromeOptions();
+       // ChromeOptions options = new ChromeOptions();
         //set fullscreen option
-        options.addArguments("start-maximized");
+        //options.addArguments("start-maximized");
 
-        driver = new ChromeDriver();
+           //driver initialisation for SBpart3
+        driver = BrowserManager.createChromeDriverWithOptions();
+        //driver initialisation for selenium grid
+        driver =  BrowserManager.createRemoteWebDriver();
         driver.get("https://www.emag.ro/");
         System.out.println("Inainte" + LocalDateTime.now());
         //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//        WebElement acceptButton = new WebDriverWait(driver, Duration.ofSeconds(5))
-//                .until(ExpectedConditions
-//                        .elementToBeClickable(By.className("js-accept")));
+       // WebElement acceptButton = new WebDriverWait(driver, Duration.ofSeconds(5))
+        //        .until(ExpectedConditions
+           //             .elementToBeClickable(By.className("js-accept")));
         Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
                 .withTimeout(Duration.ofSeconds(30))
                 .pollingEvery(Duration.ofSeconds(5))
