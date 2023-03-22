d7034ebccf3b971dea728b0c3041c4249cacb113
CosminN89 <cosneagu@gmail.com>
2023-03-23 00:03:26 +0200

    Tema7

--- src/test/java/cosminneagu/tests/tema7/BaseTest.java
+++ src/test/java/cosminneagu/tests/tema7/BaseTest.java
@@ -1,4 +1,25 @@
 package cosminneagu.tests.tema7;
 
+import cosminneagu.driver.BrowserManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
+    protected ChromeDriver driver;
+
+    @BeforeMethod(groups = {"redirect", "login"})
+    public void initChromeBrowser() {
+        driver = BrowserManager.createChromeDriver();
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+        System.out.println("Executed before method");
+    }
+
+    @AfterMethod(alwaysRun=true, groups = {"redirect", "login", "Home Button"})
+    public void quitDriver() {
+        if (driver!=null) {
+            driver.quit();
+        }
+        System.out.println("Executed after method");
+    }
 }
